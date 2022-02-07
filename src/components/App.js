8ac586218cb7e37c62dd91916eeffc019949e00c
Adham <muzaffarov.adham@gmail.com>
2022-02-07 07:19:11 +0500

    props

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagLine="Fresh seafood market" />
                 </div>
                 <Order />
                 <Inventory />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,39 @@
 import React from "react";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of">of</span>
-                        <span className="the">The</span>
-                    </span>
-                    day
-                </h1>
-                <h3 className="tagline">
-                    <span>Fresh daily</span>
-                </h3>
-            </header>
-        );
-    }
-}
+const Header = (props) => (
+    <header className="top">
+        <h1>
+            Catch
+            <span className="ofThe">
+                <span className="of">of</span>
+                <span className="the">The</span>
+            </span>
+            day
+        </h1>
+        <h3 className="tagline">
+            <span>{props.tagLine}</span>
+        </h3>
+    </header>
+);
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>
+//                     Catch
+//                     <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">The</span>
+//                     </span>
+//                     day
+//                 </h1>
+//                 <h3 className="tagline">
+//                     <span>{this.props.tagLine}</span>
+//                 </h3>
+//             </header>
+//         );
+//     }
+// }
 
 export default Header;
