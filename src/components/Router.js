ca321d5d3ecf5bc5838a9ffd0bf0ef53b90bcbdf
Adham <muzaffarov.adham@gmail.com>
2022-02-08 16:26:02 +0500

    react router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StorePicker from "./StorePicker";
+import App from "./App";
+import NotFound from "./NotFound";
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
-            <Route exact path="/" element={<StorePicker />}>
-                <StorePicker />
-            </Route>
+            <Route exact path="/" element={<StorePicker />} />
+            <Route path="/store/:storeId" element={<App />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
 );
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
     render() {
         return (
             <form className="store-selector">
                 <h1>Please, enter the store</h1>
-                <input type="text" requred placeholder="Store name"></input>
+                <input
+                    type="text"
+                    requred
+                    placeholder="Store name"
+                    defaultValue={getFunName()}
+                ></input>
                 <button type="submit">Visit Store</button>
             </form>
         );
